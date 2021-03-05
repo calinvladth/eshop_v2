cc45300ee8f9b4402955475fb34cfc8ae32e1b42
calinvladth <office@theoscoding.com>
2021-03-05 22:48:37 +0200

    pagination function fix

--- src/redux/products/services.js
+++ src/redux/products/services.js
@@ -1,10 +1,4 @@
 export const pagination = (data, limit, page) => {
-    let newData = []
     let offset = (page - 1) * limit
-
-    data.forEach((o, i) => {
-        if (i >= offset && i < offset + limit) newData.push(o)
-    })
-
-    return newData
+    return data.slice(offset, offset + limit)
 }
